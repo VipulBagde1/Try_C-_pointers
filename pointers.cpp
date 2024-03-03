055041ffd970d85aac562e0a176b4257cdc5f764
VipulBagde1 <118257328+VipulBagde1@users.noreply.github.com>
2024-03-04 00:27:32 +0530

    added code

--- pointers.cpp
+++ pointers.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-int main (){
-    
-}
+void increament(int a);
 
+int main (){
+    int a =10;
+    increament(a); // actual argument
+    std::cout<<a;
+}
+ void increament(int a /*formal argument*/){
+        a=a+1;
+ }
