237f3bfb2876b985e08ca4edbae83208713e3323
VipulBagde1 <118257328+VipulBagde1@users.noreply.github.com>
2024-03-05 23:33:47 +0530

    added pointer4

---
+++ pointer4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string.h>
+int main()
+{
+    char c[] ="JHON";
+    std::cout<<sizeof(c)<<"\n";
+    int length= strlen(c);
+    std::cout<<length;
+
+    return 0;
+}
