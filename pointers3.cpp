cd5d74b1da147995c9997fed4aaf6855ec3c5305
VipulBagde1 <118257328+VipulBagde1@users.noreply.github.com>
2024-03-05 23:11:55 +0530

    Create pointers3.cpp

---
+++ pointers3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int sumofdigit(int arr[]);
+int main (){
+
+    int arr[]={1,32,12,3,2};
+    int size1 = sizeof(arr);
+   int total= sumofdigit(arr);
+    std::cout<<total<<"\n"<<size1;
+}
+int sumofdigit(int arr[]/*it is a pointer to array*/){
+    int sum;
+    int size = sizeof(arr)/sizeof(arr[0]);//it gives error cause the array is is actually a pointer and it's size is 4
+    for(int i; i>size; i++){
+        sum = sum + arr[i];
+    }
+
+return sum;
+}
